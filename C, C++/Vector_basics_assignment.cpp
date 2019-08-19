e77471663109c56876ce13a6ed09bc7b67c754a9
seongwookchun <40543038+seongwookchun@users.noreply.github.com>
2019-08-20 00:27:59 +0900

    Create Vector_basics_assignment.cpp

---
+++ C, C++/Vector_basics_assignment.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+int main() 
+{ 
+    vector<int> vect{ 10, 20, 30 }; 
+    cout << vect.size();
+    for (int x : vect) 
+        cout << x << " "; 
+  
+    return 0; 
+} 
