c5c177084a80d8c13eb8c26b3cd4e074899d15ff
seongwookchun <40543038+seongwookchun@users.noreply.github.com>
2019-09-22 04:01:48 +0900

    Some compiler errors, then how to implement hash class
    /solution0.cpp:16:35: error: member reference base type '__gnu_cxx::__alloc_traits<std::allocator<int> >::value_type' (aka 'int') is not a structure or union
                if(mySet[hash1][hash2].empty()) mySet[hash1][hash2].resize(h_size, -1);
                   ~~~~~~~~~~~~~~~~~~~^~~~~~
    /solution0.cpp:16:64: error: member reference base type '__gnu_cxx::__alloc_traits<std::allocator<int> >::value_type' (aka 'int') is not a structure or union
                if(mySet[hash1][hash2].empty()) mySet[hash1][hash2].resize(h_size, -1);
                                                ~~~~~~~~~~~~~~~~~~~^~~~~~~
    /solution0.cpp:21:31: error: member reference base type '__gnu_cxx::__alloc_traits<std::allocator<int> >::value_type' (aka 'int') is not a structure or union
            if(mySet[hash1][hash2].empty()) return;
               ~~~~~~~~~~~~~~~~~~~^~~~~~
    /solution0.cpp:27:31: error: member reference base type '__gnu_cxx::__alloc_traits<std::allocator<int> >::value_type' (aka 'int') is not a structure or union
            if(mySet[hash1][hash2].empty()) return false;
               ~~~~~~~~~~~~~~~~~~~^~~~~~
    /solution0.cpp:27:41: error: void function 'contain' should not return a value [-Wreturn-type]
            if(mySet[hash1][hash2].empty()) return false;
                                            ^      ~~~~~
    /solution0.cpp:28:9: error: void function 'contain' should not return a value [-Wreturn-type]
            return mySet[hash1][hash2] == key;
            ^      ~~~~~~~~~~~~~~~~~~~~~~~~~~
    /solution0.cpp:31:2: error: expected ';' after class
    }
     ^
    /solution0.cpp:38:14: error: member reference type 'MyHashSet *' is a pointer; did you mean to use '->'?
            mySet.add(comp[i]);
            ~~~~~^
                 ->
    /solution0.cpp:38:19: error: no viable conversion from '__gnu_cxx::__alloc_traits<std::allocator<std::__cxx11::basic_string<char> > >::value_type' (aka 'std::__cxx11::basic_string<char>') to 'int'
            mySet.add(comp[i]);
                      ^~~~~~~
    /solution0.cpp:13:18: note: passing argument to parameter 'key' here
        void add(int key){
                     ^
    /solution0.cpp:41:17: error: member reference type 'MyHashSet *' is a pointer; did you mean to use '->'?
            if(mySet.contain(part[i]) == false) return part[i];
               ~~~~~^
                    ->
    /solution0.cpp:41:26: error: no viable conversion from '__gnu_cxx::__alloc_traits<std::allocator<std::__cxx11::basic_string<char> > >::value_type' (aka 'std::__cxx11::basic_string<char>') to 'int'
            if(mySet.contain(part[i]) == false) return part[i];
                             ^~~~~~~
    /solution0.cpp:24:22: note: passing argument to parameter 'key' here
        void contain(int key){
                         ^
    11 errors generated.
    make[2]: *** [CMakeFiles/solution_test.dir/solution0.cpp.o] Error 1
    make[1]: *** [CMakeFiles/solution_test.dir/all] Error 2
    make: *** [all] Error 2
    테스트 결과 (~˘▾˘)~
    3개 중 0개 성공

--- C, C++/Alg-Programmers/cpp, hash, participant and completion.cpp
+++ C, C++/Alg-Programmers/cpp, hash, participant and completion.cpp
@@ -3,34 +3,42 @@
 
 using namespace std;
 
-string solution(vector<string> part, vector<string> comp) {
-    string answer = "";
-    
-    vector<vector<int>> mySet();
+class MyHashSet {
+public:
     int h_size = 1000;
-    void add(key){
-        hash1 = key/h_size;
-        hash2 = key%h_size;
-        if(mySet[hash1][hash2].empty()) mySet[hash1][hash2].resize(h_size, -1);
+    MyHashSet() {
+        mySet = vector<vector<int>>(1001, vector<int>());
     }
-    void remove(key){
-        hash1 = key/h_size;
-        hash2 = key%h_size;
+    
+    void add(int key){
+            int hash1 = key/h_size;
+            int hash2 = key%h_size;
+            if(mySet[hash1][hash2].empty()) mySet[hash1][hash2].resize(h_size, -1);
+    }
+    void remove(int key){
+        int hash1 = key/h_size;
+        int hash2 = key%h_size;
         if(mySet[hash1][hash2].empty()) return;
         mySet[hash1][hash2] = -1;
     }
-    void contain(key){
-        hash1 = key/h_size;
-        hash2 = key%h_size;
+    void contain(int key){
+        int hash1 = key/h_size;
+        int hash2 = key%h_size;
         if(mySet[hash1][hash2].empty()) return false;
         return mySet[hash1][hash2] == key;
     }
+    vector<vector<int>> mySet;
+}
+
+string solution(vector<string> part, vector<string> comp) {
+    string answer = "";
+    MyHashSet* mySet = new MyHashSet();
     
     for(int i=0; i<comp.size(); i++){
-        add(comp[i]);
+        mySet.add(comp[i]);
     }
     for(int i=0; i<part.size(); i++){
-        if(contain(part[i]) == false) return part[i]
+        if(mySet.contain(part[i]) == false) return part[i];
     }
     // return answer;
 }
