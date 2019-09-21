8bf1a750e409c4c529dfd7c04ed18040efba3d96
seongwookchun <40543038+seongwookchun@users.noreply.github.com>
2019-09-22 02:40:29 +0900

    Update Vector_basics_assignment.cpp
    Add: A simple way to assign initial value in an vector object.

--- C, C++/Vector_basics_assignment.cpp
+++ C, C++/Vector_basics_assignment.cpp
@@ -4,7 +4,23 @@ using namespace std;
 int main() 
 { 
     vector<int> vect{ 10, 20, 30 }; 
-    cout << vect.size();
+    cout << vect.size() << " ";
+    for (int x : vect) 
+        cout << x << " "; 
+  
+    return 0; 
+} 
+
+// or simply
+
+#include <iostream>
+#include <vector> 
+using namespace std; 
+  
+int main() 
+{ 
+    vector<int> vect{ 10, 20, 30 }; 
+    cout << vect.size() << " ";
     for (int x : vect) 
         cout << x << " "; 
   
