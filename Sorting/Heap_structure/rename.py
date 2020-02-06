4fbba134652a46669b4ab991c28d217f05bdf1d7
seongwookchun <40543038+seongwookchun@users.noreply.github.com>
2020-02-06 11:50:00 +0900

    Add files via upload

---
+++ Sorting/Heap_structure/rename.py
@@ -0,0 +1,14 @@
+﻿# importing os module 
+import os 
+from glob import glob
+# Function to rename multiple files 
+      
+for idx, filename in enumerate(glob("./*.png")):
+    dst ="formingHeapStructure_" + '{:03d}'.format(idx) + ".png"
+    src =filename 
+    # dst ='xyz'+ dst 
+          
+    # rename() function will 
+    # rename all the files 
+    # os.rename(src, dst) 
+    print('{:03d}'.format(idx), filename)
---
+++ Sorting/Heap_structure/rename01.py
@@ -0,0 +1,22 @@
+﻿# importing os module 
+import os 
+from glob import glob
+import re
+# Function to rename multiple files 
+
+d_reordering = dict()
+for idx, filename in enumerate(glob("./*.png")):
+	num = re.findall('\d+', filename)[0]
+	d_reordering[int(num)] = filename
+    # dst ="formingHeapStructure_" + '{:03d}'.format(idx) + ".png"
+    # src =filename
+    # # dst ='xyz'+ dst
+          
+    # # rename() function will 
+    # # rename all the files 
+    # # os.rename(src, dst) 
+    # print('{:03d}'.format(idx), filename)
+
+sorted(d_reordering)#, key=lambda x: x[0])
+
+print(d_reordering)
